026b8fd85289b9f466a7236e9aaed5756b53dc5e
Damir Krkalic <damir.krkalic@tob.ba>
2021-10-25 22:50:51 +0200

    initalize

---
+++ OnboardingSoftware.App/OnboardingSoftware.App/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using OnboardingSoftware.App.ViewModels;
+using OnboardingSoftware.App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace OnboardingSoftware.App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ OnboardingSoftware.App/OnboardingSoftware.App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using OnboardingSoftware.App.Models;
+using OnboardingSoftware.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OnboardingSoftware.App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
