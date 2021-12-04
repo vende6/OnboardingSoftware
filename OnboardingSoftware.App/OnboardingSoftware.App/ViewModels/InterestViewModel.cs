745a7b8ba0b82825a267976c87c92908f441fc35
Damir Krkalić <damirkrkalic@source.no>
2021-12-04 23:44:37 +0100

    ui;

---
+++ OnboardingSoftware.App/OnboardingSoftware.App/ViewModels/InterestViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.MultiSelectListView;
+
+namespace OnboardingSoftware.App.ViewModels
+{
+    public class InterestViewModel : BaseViewModel
+    {
+        public class Interes
+        {
+            public string ID { get; set; }
+            public string Naziv { get; set; }
+            public string IsSelected { get; set; }
+        }
+
+        public MultiSelectObservableCollection<Interes> Interesi { get; }
+
+        public ICommand DisplayNameCommand => new Command<Interes>(async interes =>
+        {
+            await Application.Current.MainPage.DisplayAlert("Selected Name", interes.Naziv, "OK");
+        });
+
+        public ICommand FinishCommand => new Command<Interes>(async interes =>
+        {
+            Debug.WriteLine(Interesi);
+            Debug.WriteLine("---------- OnStart called!");
+        });
+
+        public InterestViewModel()
+        {
+            Interesi = new MultiSelectObservableCollection<Interes>();
+
+            Interes interes = new Interes();
+            interes.Naziv = "Bertuzzi1";
+            Interesi.Add(interes);
+
+            Interesi[0].IsSelected = true;
+
+            interes = new Interes();
+            interes.Naziv = "Bertuzzi2";
+            Interesi.Add(interes);
+
+            interes = new Interes();
+            interes.Naziv = "Bertuzzi3";
+            Interesi.Add(interes);
+
+        }
+    }
+}
---
+++ OnboardingSoftware.App/OnboardingSoftware.App/Views/ViewSkill.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.MultiSelectListView;
+using Xamarin.Forms.Xaml;
+
+namespace OnboardingSoftware.App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewSkill : ContentPage
+    {
+
+        public ViewSkill()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
