da81220a0c6899824e56f0a6756fda89700aa893
Damir Krkalić <damirkrkalic@source.no>
2021-12-19 01:12:10 +0100

    app;

--- OnboardingSoftware.App/OnboardingSoftware.App/Resources/OdgovorResource.cs
+++ OnboardingSoftware.App/OnboardingSoftware.App/Resources/OdgovorResource.cs
@@ -14,10 +14,62 @@ namespace OnboardingSoftware.App.Resources
         public string PonudjeniOdgovor_2 { get; set; }
         public string PonudjeniOdgovor_3 { get; set; }
         public string PonudjeniOdgovor_4 { get; set; }
-        public bool TacanOdgovor_1 { get; set; }
-        public bool TacanOdgovor_2 { get; set; }
-        public bool TacanOdgovor_3 { get; set; }
-        public bool TacanOdgovor_4 { get; set; }
+
+        private bool _tacanOdgovor_1;
+        public bool TacanOdgovor_1
+        {
+            get
+            {
+                return _tacanOdgovor_1;
+            }
+            set
+            {
+                _tacanOdgovor_1 = value;
+                RaisePropertyChanged(() => TacanOdgovor_1);
+            }
+        }
+
+        private bool _tacanOdgovor_4;
+        public bool TacanOdgovor_4
+        {
+            get
+            {
+                return _tacanOdgovor_4;
+            }
+            set
+            {
+                _tacanOdgovor_4 = value;
+                RaisePropertyChanged(() => TacanOdgovor_4);
+            }
+        }
+
+        private bool _tacanOdgovor_2;
+        public bool TacanOdgovor_2
+        {
+            get
+            {
+                return _tacanOdgovor_2;
+            }
+            set
+            {
+                _tacanOdgovor_2 = value;
+                RaisePropertyChanged(() => TacanOdgovor_2);
+            }
+        }
+
+        private bool _tacanOdgovor_3;
+        public bool TacanOdgovor_3
+        {
+            get
+            {
+                return _tacanOdgovor_3;
+            }
+            set
+            {
+                _tacanOdgovor_3 = value;
+                RaisePropertyChanged(() => TacanOdgovor_3);
+            }
+        }
 
         public ICommand MarkAsCorrect_1Command => new Command<OdgovorResource>(async odgovor =>
         {
