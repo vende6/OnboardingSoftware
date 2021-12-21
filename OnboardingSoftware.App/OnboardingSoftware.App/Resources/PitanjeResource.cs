f749c28df8d8ab828999bc3990dac4f4c0eeefae
Damir Krkalić <damirkrkalic@source.no>
2021-12-21 07:47:38 +0100

    app;

--- OnboardingSoftware.App/OnboardingSoftware.App/Resources/PitanjeResource.cs
+++ OnboardingSoftware.App/OnboardingSoftware.App/Resources/PitanjeResource.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace OnboardingSoftware.App.Resources
 {
@@ -16,6 +17,27 @@ namespace OnboardingSoftware.App.Resources
         public string Test { get; set; }
         public bool IsLast { get; set; }
         public int Position { get; set; }
+
+
+        private Color _color;
+        public Color Color
+        {
+            get
+            {
+                if(RedniBroj.Contains("1"))
+                return Color.FromHex("#e16f62");
+                else if (RedniBroj.Contains("2"))
+                    return Color.FromHex("#e16f62");
+                else
+                return Color.FromHex("##56cd89");
+            }
+            set
+            {
+                _color = value;
+                RaisePropertyChanged(() => Color);
+            }
+        }
+
 
         private ObservableCollection<OdgovorResource> _odgovori = new ObservableCollection<OdgovorResource>();
         public ObservableCollection<OdgovorResource> Odgovori
