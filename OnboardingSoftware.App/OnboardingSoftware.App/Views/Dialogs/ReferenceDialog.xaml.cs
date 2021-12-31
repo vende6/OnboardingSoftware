366f4b3d248035e8cc599115a8b5b07925f2e27e
Damir Krkalić <damirkrkalic@source.no>
2021-12-31 02:36:15 +0100

    app;

--- OnboardingSoftware.App/OnboardingSoftware.App/Views/Dialogs/ReferenceDialog.xaml.cs
+++ OnboardingSoftware.App/OnboardingSoftware.App/Views/Dialogs/ReferenceDialog.xaml.cs
@@ -1,11 +1,11 @@
-﻿using Plugin.FilePicker;
+﻿
 using Rg.Plugins.Popup.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -25,11 +25,30 @@ namespace OnboardingSoftware.App.Views.Dialogs
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            var file = await CrossFilePicker.Current.PickFile();
+            //var file = await CrossFilePicker.Current.PickFile();
 
-            if (file != null)
+            //if (file != null)
+            //{
+            //    lbl.Text = file.FileName;
+            //}
+            try
             {
-                lbl.Text = file.FileName;
+                var result = await FilePicker.PickAsync(PickOptions.Default);
+                if (result != null)
+                {
+                    lbl.Text = $"File Name: {result.FileName}";
+                    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var stream = await result.OpenReadAsync();
+                        //  Image = ImageSource.FromStream(() => stream);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var exx = ex;
+                throw;
             }
         }
     }
