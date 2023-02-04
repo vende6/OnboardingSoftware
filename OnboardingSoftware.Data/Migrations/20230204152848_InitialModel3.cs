05c398d8721abdd00d6b9ad67cab77a0e433e3d0
dkrkalic <damirkrkalic11@gmail.com>
2023-02-04 19:04:47 +0100

    initalize;

---
+++ OnboardingSoftware.Data/Migrations/20230204152848_InitialModel3.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnboardingSoftware.Data.Migrations
+{
+    public partial class InitialModel3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
