6a774df82ad37867cc403f61f1e6a5dfa70d6969
dkrkalic <damirkrkalic11@gmail.com>
2023-02-05 20:04:35 +0100

    packages;

--- OnboardingSoftware.Api/Program.cs
+++ OnboardingSoftware.Api/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace OnboardingSoftware.Api
 {
@@ -18,6 +19,11 @@ namespace OnboardingSoftware.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
